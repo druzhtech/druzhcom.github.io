9a7346058bcef3474149853ae0e5b49fe359c88b
Ilya Druzhinin <ilya.druzh@gmail.com>
2021-02-13 20:40:30 +0300

    добавил шрифты

--- site_src/src/App.js
+++ site_src/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+// import logo from './logo.svg';
 import {YMInitializer} from 'react-yandex-metrika';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Дружининъ</h1>
-        <p>Исследования и разработка</p>
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>Дружинин<span class="red-letter">ъ</span></h1>
+        {/* <small></small> */}
         <YMInitializer accounts={[72383356]} options={{webvisor: true}}/>
       </header>
     </div>
