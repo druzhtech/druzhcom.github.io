9561cc70c7e3ebe4fa242d4c5402c6249d078c7d
Ilya Druzhinin <ilya.druzh@gmail.com>
2021-02-13 12:13:57 +0300

    init

--- README.md
+++
@@ -1,1 +0,0 @@
-# druzhcom.github.io
---
+++ site_src/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Дружининъ</h1>
+        <p>Исследования и разработка</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
